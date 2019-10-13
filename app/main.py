61e4534c9a461d3edaddbdcfed65dd9695f2c069
rocketpy <dimitr.ushakov@gmail.com>
2019-10-13 21:39:03 +0300

    updated

--- app/main.py
+++ app/main.py
@@ -1,17 +1,37 @@
 from flask import Flask, render_template
+from flask.ext.sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 
 
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:postgres1@localhost/DataCollector'
+db = SQLAlchemy(app)
+
+class Data(db.Model):
+    #create a table
+    __tablename__ = "data"
+    id = db.Column(db.Integer, primary_key = True)
+    height = db.Column(db.Integer)
+    weight = db.Column(db.Integer)
+    shoesize = db.Column(db.Integer)
+    sex = db.Column(db.String)
+
+    def __init__(self, height, weight, shoesize, sex):
+        self.height = height
+        self.weight = weight
+        self.shoesize = shoesize
+        self.sex = sex
+
+
 @app.route("/")
-def home():
-    return render_template("home.html")
+def index():
+    return render_template("index.html")
 
 
-@app.route("/blog")
-def about():
-    return render_template("blog.html")
+@app.route("/success", methods = ['POST'])
+def success():
+    return render_template("success.html")
 
 
 if (__name__ =="__main__"):
